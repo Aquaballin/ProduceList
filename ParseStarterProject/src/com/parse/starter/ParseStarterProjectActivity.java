da42e558bb8a5f456aad65a364caccc5737f9254
David Casey <caseydj@dukes.jmu.edu>
2015-04-17 19:58:22 -0400

    Initial commit

---
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -0,0 +1,24 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.parse.ParseAnalytics;
+
+public class ParseStarterProjectActivity extends Activity {
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login_activity);
+
+
+
+
+
+
+
+        
+
+		ParseAnalytics.trackAppOpenedInBackground(getIntent());
+	}
+}
