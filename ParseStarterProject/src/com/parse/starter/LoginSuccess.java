644092aa84553e917e91f40fab1124828e12fd6e
David Casey <caseydj@dukes.jmu.edu>
2015-04-17 22:11:21 -0400

    oh god

---
+++ ParseStarterProject/src/com/parse/starter/LoginSuccess.java
@@ -0,0 +1,39 @@
+package com.parse.starter;
+
+import com.parse.ParseUser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginSuccess extends Activity {
+    Button logout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.login_success);
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        String struser = currentUser.getUsername().toString();
+
+        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+
+        txtuser.setText("You are logged in as " + struser);
+
+        logout = (Button) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                ParseUser.logOut();
+                finish();
+            }
+        });
+    }
+}
