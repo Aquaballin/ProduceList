fb046ed47e9122c5a7d5158faca6e8b24cf20186
David Casey <caseydj@dukes.jmu.edu>
2015-04-17 20:23:25 -0400

    Fri 8:07 added values and shit Fri 8:22 blank slate, lets add a login

--- ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,24 +1,34 @@
 package com.parse.starter;
-
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.parse.ParseAnalytics;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
+/**
+ * Activity which displays a login screen to the user, offering registration as well.
+ */
 public class ParseStarterProjectActivity extends Activity {
-	/** Called when the activity is first created. */
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.login_activity);
+    // UI references.
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.login_activity);
 
-
-
-
-
-        
-
-		ParseAnalytics.trackAppOpenedInBackground(getIntent());
-	}
+        // Set up the login form.
+    }
 }
