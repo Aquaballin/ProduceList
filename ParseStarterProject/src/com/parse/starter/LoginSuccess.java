61867983138c50bccef1f09fff3008aa7c8c4fa0
David Casey <caseydj@dukes.jmu.edu>
2015-04-17 22:44:41 -0400

    LOGIN COMPLETE V2

--- ParseStarterProject/src/com/parse/starter/LoginSuccess.java
+++ ParseStarterProject/src/com/parse/starter/LoginSuccess.java
@@ -1,5 +1,6 @@
 package com.parse.starter;
 
+import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 
 import android.app.Activity;
@@ -10,23 +11,36 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginSuccess extends Activity {
+
     Button logout;
+    private ParseQueryAdapter<Post> queryAdapter;
+    private ParseQueryAdapter.QueryFactory<Post> queryRequirements;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.login_success);
-
         ParseUser currentUser = ParseUser.getCurrentUser();
-
         String struser = currentUser.getUsername().toString();
-
         TextView txtuser = (TextView) findViewById(R.id.txtuser);
-
         txtuser.setText("You are logged in as " + struser);
-
         logout = (Button) findViewById(R.id.logout);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         logout.setOnClickListener(new OnClickListener() {
 
