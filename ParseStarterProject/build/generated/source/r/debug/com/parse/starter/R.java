9f78fb36662b243c7d2369510fd9906342f7c791
liam <themailman05@gmail.com>
2015-04-18 22:38:27 -0400

    added email to login activity

--- ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
+++ ParseStarterProject/build/generated/source/r/debug/com/parse/starter/R.java
@@ -11,20 +11,54 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int button=0x7f06000b;
+        public static final int email=0x7f060003;
+        public static final int list=0x7f060009;
+        public static final int login=0x7f060006;
+        public static final int logout=0x7f06000a;
+        public static final int password=0x7f060005;
+        public static final int ptr_id_header=0x7f06000c;
+        public static final int ptr_id_image=0x7f06000d;
+        public static final int ptr_id_last_updated=0x7f060011;
+        public static final int ptr_id_spinner=0x7f06000e;
+        public static final int ptr_id_text=0x7f060010;
+        public static final int ptr_id_textwrapper=0x7f06000f;
+        public static final int signup=0x7f060007;
+        public static final int txtemail=0x7f060002;
+        public static final int txtpassword=0x7f060004;
+        public static final int txtuser=0x7f060008;
+        public static final int txtusername=0x7f060000;
+        public static final int username=0x7f060001;
     }
     public static final class layout {
         public static final int login_activity=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int login_signup=0x7f030001;
+        public static final int login_success=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int ptr_header=0x7f030004;
     }
     public static final class string {
-        public static final int LogoutBtn=0x7f040000;
-        public static final int Username=0x7f040001;
-        public static final int Welcome=0x7f040002;
-        public static final int app_name=0x7f040003;
-        public static final int ptr_last_updated=0x7f040004;
-        public static final int ptr_pull_to_refresh=0x7f040005;
-        public static final int ptr_refreshing=0x7f040006;
-        public static final int ptr_release_to_refresh=0x7f040007;
+        public static final int Email=0x7f040000;
+        public static final int LogoutBtn=0x7f040001;
+        public static final int Username=0x7f040002;
+        public static final int Welcome=0x7f040003;
+        public static final int app_name=0x7f040004;
+        public static final int ptr_last_updated=0x7f040005;
+        public static final int ptr_pull_to_refresh=0x7f040006;
+        public static final int ptr_refreshing=0x7f040007;
+        public static final int ptr_release_to_refresh=0x7f040008;
+    }
+    public static final class style {
+        public static final int ptr_arrow=0x7f050000;
+        public static final int ptr_header=0x7f050001;
+        public static final int ptr_headerContainer=0x7f050002;
+        public static final int ptr_last_updated=0x7f050003;
+        public static final int ptr_spinner=0x7f050004;
+        public static final int ptr_text=0x7f050005;
+        public static final int ptr_textwrapper=0x7f050006;
     }
 }
